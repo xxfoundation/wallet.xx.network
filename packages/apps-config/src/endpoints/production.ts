cb870e5b34713cdcab33e8b10b64eb3e23175e49
Keith Millette <keith@xx.network>
2026-09-02 11:39:42 +0900

    Remove Dwellir RPC endpoint

--- packages/apps-config/src/endpoints/production.ts
+++ packages/apps-config/src/endpoints/production.ts
@@ -728,8 +728,7 @@ export const prodChains: Omit<EndpointOption, 'teleport'>[] = [
     info: 'xxnetwork',
     isPeopleForIdentity: false,
     providers: {
-      'xx foundation': 'wss://rpc.xx.network',
-      dwellir: 'wss://xxnetwork-rpc.n.dwellir.com'
+      'xx foundation': 'wss://rpc.xx.network'
     },
     text: 'xx network',
     ui: {
